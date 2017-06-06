ea641261d85af8a0826dbdeb06c5d2f4111d72aa
Victor Zhu <zzy809@126.com>
2017-06-06 12:07:36 -0400

    Updated Setup Script and requirements.txt

--- bin/setup.sh
+++ bin/setup.sh
@@ -18,6 +18,17 @@ sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with passw
 # Create the database
 sudo -u postgres createdb django_git_tinymce
 
+# Install libgit2
+wget https://github.com/libgit2/libgit2/archive/v0.25.1.tar.gz
+tar xzf v0.25.1.tar.gz
+cd libgit2-0.25.1/
+cmake .
+make
+sudo make install
+
 # Install libraries for the community site
 pip3 install --upgrade pip
 pip3 install --upgrade -r /vagrant/requirements.txt
+
+# ligbgit2-pygit2 dynamic linker 
+sudo ldconfig
