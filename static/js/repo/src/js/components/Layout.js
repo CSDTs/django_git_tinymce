53f94a8e2abef2b7f1b73be23cce59faace00a11
holmr <holmr@rpi.edu>
2017-08-31 11:56:02 -0400

    Working product with directory

--- static/js/repo/src/js/components/Layout.js
+++ static/js/repo/src/js/components/Layout.js
@@ -131,6 +131,39 @@ export default class Layout extends React.Component {
 
     const editShow = (files.is_owner) ? <a href={`setting/`} style={{color: '#999', fontSize: '.75em'}}>edit</a> : null
 
+    // function folderShow() {
+    //   const directory = window.props.directory
+    //   let dir_split = directory.split("/")
+    //   let working_dir = ""
+    //   let working_html = ""
+    //   dir_split.forEach((folder) => {
+    //     working_dir += folder + "/"
+    //     working_html.push(<font>/ <a href={`/${window.props.owner_name}/${window.props.repo_name}/${working_dir}`}>{folder}</a></font>)
+    //   })
+    //   console.log('working_html', working_html)
+    //   return working_html
+    // }
+
+    const dir_array = window.props.directory.split("/")
+
+
+    function folders() {
+      let working_dir = ""
+      let last = dir_array.length - 1
+      const arrayList = dir_array.map((folder, i) => {
+        working_dir += folder
+        let words = null
+        if (i == last) {
+            words = <font>/ {folder} </font>
+        }
+        else {
+            words = <font>/ <a href={`/${window.props.repo_owner}/${window.props.repo_name}/${working_dir}`}>{folder}</a> </font>
+        }
+        working_dir += "/"
+        return words
+      })
+      return arrayList
+    }
 
     if (!this.props.files.fetched) {
             return <p>Loading…</p>;
@@ -179,9 +212,12 @@ export default class Layout extends React.Component {
           <p>
             <h4 className="repo-header">{repo.description} {editShow}</h4>
 
+
           </p>
 
-          <Branches branches={files.branches} is_owner={files.is_owner}/>
+          <Branches branches={files.branches} is_owner={files.is_owner} />
+
+          <p className="dir-tree"><a href={`/${window.props.repo_owner}/${window.props.repo_name}`}>{`${window.props.repo_name}`}</a> {folders()}</p>
 
           <div class="panel panel-success">
             {(files.committer) ? <div class="panel-heading">Last commit message by <a href={`/${files.committer}`}>{files.committer}</a>: <a href={`commit/${files.hex}`}>{files.message} <font style={{color: '#999'}}><i>({this.timeFormat(files.time)})</i></font></a></div> : <div class="panel-heading">No Files Yet</div> }
