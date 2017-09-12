e835c561584e5b8cdfa47be15a36c3303217c358
Ryan D Holm <ryaholm@users.noreply.github.com>
2017-09-12 14:59:20 -0400

    Pagination for menu (#43)

--- static/js/menu/src/js/components/Layout.js
+++ static/js/menu/src/js/components/Layout.js
@@ -1,18 +1,45 @@
 import React from "react"
 import axios from "axios";
 import { connect } from "react-redux"
+import { Table, Pagination } from 'react-bootstrap'
+import { push } from 'react-router-redux'
 
 import { fetchRepos } from "../actions/reposActions"
 import { fetchTags } from "../actions/tagsActions"
+import { routerActions } from 'react-router-redux';
+import queryString from 'query-string'
+import { withRouter } from 'react-router'
+
+
+
+
+
+
 
 
 @connect((store) => {
+
   return {
     repos: store.repos.repos,
-    tags: store.tags.tags
+    tags: store.tags.tags,
+    page: 1,
+    //page: Number(store.routing.locationBeforeTransitions.query.page) || 1
   };
 })
+
+
+
 export default class Layout extends React.Component {
+
+
+
+
+
+  constructor(props) {
+    super(props);
+    this.changePage = this.changePage.bind(this);
+  }
+
   componentWillMount() {
     //this.props.dispatch(fetchUser())
     this.props.dispatch(fetchRepos())
@@ -37,7 +64,25 @@ export default class Layout extends React.Component {
   }
 
   render() {
+
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+    let query = getParameterByName('page') || 1
+    console.log(query, 'query')
     const { user, repos, fetching, fetched, tags } = this.props;
+    const per_page = 20;
+    const pages = Math.ceil(this.props.repos.length / per_page );
+    const current_page = query
+    console.log('current_page', current_page)
+    const start_offset = (current_page - 1) * per_page;
+    let start_count = 0;
 
     // if (!repos.length) {
     //   return <button onClick={this.fetchRepos.bind(this)}>load repos</button>
@@ -45,8 +90,13 @@ export default class Layout extends React.Component {
 
     //const mappedTweets = tweets.map(tweet => <li key={tweet.id}>{tweet.text}</li>)
 
-    const mappedRepos = this.props.repos.map(repo => {
-      return  <a href={`/${repo.owner_username}/${repo.name}`.toLowerCase()} key={repo.id}><div className="col-md-4 box" ><img src={repo.photo_url} width="100%" className="img-responsive"/>{repo.name} by { repo.owner_username }</div></a>
+    const mappedRepos = this.props.repos.map((repo, index) => {
+      if (index >= start_offset && start_count < per_page) {
+        start_count++;
+        console.log('start_count', start_count)
+        return  <a href={`/${repo.owner_username}/${repo.name}`.toLowerCase()} key={repo.id}><div className="col-md-4 box" ><img src={repo.photo_url} width="100%" className="img-responsive"/>{repo.name} by { repo.owner_username }</div></a>
+      }
+
     })
 
     const mappedTags = this.props.tags.map(tag => {
@@ -56,7 +106,10 @@ export default class Layout extends React.Component {
       }
       let titled = toTitleCase(tag.title)
       console.log('titled', titled)
+
       return <li key={tag.id}><a href="#" >{titled}</a></li>
+
+
     })
 
     return <div>
@@ -70,16 +123,27 @@ export default class Layout extends React.Component {
           <ul style={{listStyle: 'none', fontSize: '.90em'}}>
             Tags
             {mappedTags}
+
           </ul>
         </div>
         <div className="col-md-10">
           <div className="row">
             {mappedRepos}
-          </div>
+            <br/>
 
+          </div>
+          <div className="row">
+            <div className="col-md-12 text-center">
+              <Pagination className="user-pagination" bsSize="medium" maxButtons={10} first last next prev boundaryLinks items={pages} activePage={current_page} onSelect={this.changePage}/>
+            </div>
+          </div>
         </div>
       </div>
 
     </div>
+  }
+
+  changePage(page) {
+    this.props.history.push('/?page=' + page);
   }
 }
--- static/js/menu/src/js/store.js
+++ static/js/menu/src/js/store.js
@@ -5,7 +5,15 @@ import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
 
 import reducer from "./reducers"
+import { routerMiddleware, push } from 'react-router-redux'
+import {browserHistory,withRouter} from 'react-router-dom';
+import history from './history';
 
-const middleware = applyMiddleware(promise(), thunk, logger())
+
+
+
+const middlewareHistory = routerMiddleware(history)
+
+const middleware = applyMiddleware(promise(), thunk, logger(), middlewareHistory)
 
 export default createStore(reducer, middleware)
