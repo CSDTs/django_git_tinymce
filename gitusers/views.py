75260f63cd99e28c05f58151b53bf642edd2d238
holmr <holmr@rpi.edu>
2017-10-02 15:43:21 -0400

    Flake8

--- gitusers/views.py
+++ gitusers/views.py
@@ -472,7 +472,10 @@ class RepositoryCreateFileView(OwnerRequiredMixin, FormView):
                 form.add_error("filename", "path already exists")
                 return self.form_invalid(form)
             else:
-                form.add_error("filename", "Do you want a folder or a file? Add extension like '.html' if file creation, or if adding a directory, add a placeholder file too. Example: folder1name/folder2name/placeholder.html")
+                form.add_error("filename", "Do you want a folder or a file? \
+                Add extension like '.html' if file creation, or if adding a \
+                directory, add a placeholder file too. Example: \
+                folder1name/folder2name/placeholder.html")
                 return self.form_invalid(form)
 
         try:
